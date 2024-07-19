43dcaab3f3665ccec6db6cb2a935e81bd81c43fc
Sneha Reddy <snehareddy9178@gmail.com>
2024-07-19 13:46:09 +0530

    remove unused namespaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
-import Button from './components/Button';
-import Carousel from './components/Carousel';
-import Promotion from './components/Promotion';
 import Sidebar from './components/Sidebar';
-import Reactangels from './components/Reactangels';
-import ImageCarousel from './components/ImageCarousel';
 import ProfileCard from './components/ProfileCard';
 import { Route, Routes } from 'react-router-dom';
-import { Product } from './components/Product';
-import MobileNavigation from './components/MobileNavigation';
-import HotelBooking from './components/HotelBooking';
-import ShoppingList from './components/ShoppingList';
-import UploadImage from './components/UploadImage';
-
-const slides = [
-  'https://images.pexels.com/photos/459203/pexels-photo-459203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  'https://images.pexels.com/photos/381739/pexels-photo-381739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  'https://images.pexels.com/photos/462162/pexels-photo-462162.jpeg',
-  'https://images.pexels.com/photos/547115/pexels-photo-547115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  'https://images.pexels.com/photos/1766838/pexels-photo-1766838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  'https://images.pexels.com/photos/460775/pexels-photo-460775.jpeg?auto=compress&cs=tinysrgb&w=600',
-];
 
 function App() {
   const [darkTheme] = useState(true);
